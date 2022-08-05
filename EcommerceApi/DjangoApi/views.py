f16f85b21cf9492614cd345e702fa075a77fc26e
Rochak <rochakbhattarai11@gmail.com>
2022-08-06 00:40:53 +0530

    Initial

---
+++ EcommerceApi/DjangoApi/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+#from EcommerceApi.DjangoApi.models import Matches
+from .models import Matches
+from .serializers import MatchesSerializer
+
+# Create your views here.
+class ListMatches(generics.ListCreateAPIView):
+    queryset = Matches.objects.all()
+    serializer_class = MatchesSerializer
+
+class DetailMatches(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Matches.objects.all()
+    serializer_class = MatchesSerializer
